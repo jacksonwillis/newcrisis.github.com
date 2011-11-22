66fd2136d7f499c45aed55e28395a35e7c0b618d
Jackson Willis <railsmail42@gmail.com>
2011-11-22 12:40:56 +0000

    Finished Sadducees level

--- src/js/newcrisis.js
+++ src/js/newcrisis.js
@@ -1,27 +1,39 @@
 var hashTo, newGame, flash;
 
-document.observe("dom:loaded", function() {
-  var i, _results;
-  if (!window.location.hash) {
-    _results = [];
-    for (i = 1; i <= 4; i++) {
-      _results.push($("choice-" + i).observe('click', function(e) {
-	window.location.hash = i;//Event.element(e).id.split('choice-')[1];
-        newGame();
-      }));
-    }
-    return _results;
-  } else {
-    return newGame();
-  }
-});
-
 flash = function(s) {
   $('flash').innerHTML = s + "\n" + $('flash').innerHTML;
 }
 
-newGame = function() {
-  var mode;
+var pharisses, sadducees, monastics, zealots, sadduceesLose;
+
+pharisses = function () {
+  
+}
+
+sadducees = function () {
+  $('status').innerHTML = "<button onclick='sadduceesLose();' class='diss'>Diss the Romans</button>";
+  $('action').innerHTML += "<img src='src/img/you.png' id='you'>";
+}
+
+sadduceesLose = function(){
+  $('you').setAttribute('src', 'src/img/you-romans-suck.png');
+  flash('What have you done?');
+  setTimeout('$("action").innerHTML += "<img src=\'src/img/what-you-say.png\' id=\'what\'>";', 1500);
+  setTimeout('$("newcrisis").innerHTML = ""; $("newcrisis").style.background = \'url(src/img/dead.png)\'', 3500);
+}
+
+monastics = function () {
+
+}
+
+rebels = function () {
+
+}
+
+newGame = function(mode) {
   $('newgame').hide();
-  mode = window.location.hash.substring(1);
+  if(mode == '1') { flash('You have joined the Sadducees.'); sadducees(); }
+  if(mode == '2') { flash('You have joined the Pharisses.'); pharisses(); }
+  if(mode == '3') { flash('You have joined the Monastics.'); monastics(); }
+  if(mode == '4') { flash('You have joined the Zealots.'); zealots(); }
 };
